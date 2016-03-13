3484059e6ec5dcf34102d440e4807643a724896b
J. Holmes <jholmes@n.io>
2016-03-13 07:33:17 -0600

    initial commit

---
+++ brunch-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  paths: { public: 'public', watched: ['app', 'node_modules/nio-scss/scss'] },
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(?!app\/)/,
+        'app.js': /^app\//
+      }
+    },
+    stylesheets: { joinTo: 'app.css' }
+  },
+  server: {
+    hostname: '0.0.0.0',
+    port: 8080,
+    noPushState: true,
+  },
+  plugins: {
+    eslint: {
+      pattern: /^app\/.*\.js$/
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')(['last 2 versions'])
+      ]
+    },
+    sass: {
+      mode: 'native',
+      options: { includePaths: ['node_modules/nio-scss/scss'] },
+      functions: require('nio-scss')
+    },
+    static: {
+      processors: [
+        require('html-brunch-static')({
+          handlebars: { enableProcessor: true },
+          processors: [
+            require('marked-brunch-static')()
+          ]
+        })
+      ]
+    }
+  }
+}
