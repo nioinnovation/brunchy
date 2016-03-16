816a49f489a2aaf9fe7c28d8f7cffe826fa929bd
J. Holmes <jholmes@n.io>
2016-03-16 11:44:32 -0600

    thrashing and starting to extract out the framework proper.

--- helpers/hbs-blog-list.js
+++ helpers/hbs-blog-list.js
@@ -1,25 +1,52 @@
 var fs = require('fs');
+var path = require('path');
 var hbs = require('handlebars');
+var jsyaml = require('yaml-front-matter');
+var moment = require('moment');
 
 var isTimeStamp = /^\d{8}/;
 var isStatic = /\.static\.\w+$/;
 
-var sortDesc = (a,b) => (a === b) ? 0 : (a < b) ? 1 : -1;
+var inputPath = './app/blog';
+var outputPath = '/blog';
+
+var sortDesc = (a,b) => (a.posted === b.posted) ? 0 : (a.posted < b.posted) ? 1 : -1;
+
+var tmpl = hbs.compile(`
+<div class="blog-summary">
+  <h2 class="blog-summary__title"><a href='{{href}}'>{{title}}</a></h2>
+  <ul class="blog-summary__byline">
+    {{~#if author}}<li>by {{author}}</li>{{/if~}}
+    {{~#if posted}}<li>posted {{postedFormated}}</li>{{/if~}}
+  </ul>
+  {{#if summary}}
+  <p class="blog-summary__summary">
+    {{summary}}
+    <a href="{{href}}" class="blog-summary__read-more">Read more&hellip;</a>
+  </p>
+  {{/if}}
+</div>`);
 
 module.exports = function() {
 
-  var posts = fs.readdirSync('./app/blog')
-    .filter( f => isStatic.test(f) )
-    .filter( f => isTimeStamp.test(f) );
+  var posts = fs.readdirSync(inputPath)
+    .filter( filename => isStatic.test(filename) )
+    .filter( filename => isTimeStamp.test(filename) )
+    .map( filename => path.join(inputPath, filename) )
+    .map( filename => ({ buffer: fs.readFileSync(filename, "utf8"), filename: filename }) )
+    .map( buffer   => ({ meta: jsyaml.loadFront(buffer.buffer), filename: buffer.filename } ))
+    .map( parsed   => ({
+      href: path.join(outputPath, path.basename(parsed.filename.replace(isStatic, '.html'))),
+      title: parsed.meta.title || parsed.filename.replace(isTimeStamp, '').replace(isStatic, '').replace(/-/g, ' '),
+      posted: +moment(parsed.meta.posted || parsed.filename.match(isTimeStamp), "YYYYMMDD"),
+      postedFormated: moment(parsed.meta.posted || parsed.filename.match(isTimeStamp), "YYYYMMDD").format('LL'),
+      summary: parsed.meta.summary,
+      author: parsed.meta.author
+    }) );
 
   // Sort into the order
   posts.sort(sortDesc);
 
-  var result = posts.map(function(fn) {
-    var renderedFn = fn.replace(isStatic, '.html');
-    var shortname = fn.replace(isTimeStamp, '').replace(isStatic, '').replace(/-/g, ' ');
-    return `<li><a href='/blog/${renderedFn}'>${shortname}</a></li>`;
-  }).join('');
-
+  var result = posts.map(tmpl).join('');
   return new hbs.SafeString(result);
 }
